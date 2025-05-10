e6a86295a7c88cb639c408e8bb1186ce99f0d0f7
riya-jangam <jangam.riyaa@gmail.com>
2025-05-10 15:58:32 +0530

    Update Assignment1_04.py

--- Marvellous_Python_Assignments/Assignment1/Assignment1_04.py
+++ Marvellous_Python_Assignments/Assignment1/Assignment1_04.py
@@ -1,9 +1,3 @@
-def display():
-    for no in range(5) :
-        print("Marvellous")
+for no in range(5) :            
+    print("Marvellous")
 
-main():
-    dispaly()
-
-if __name__ == "___main__" :
-    main()
